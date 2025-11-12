de007e746374563b076d476bc4d79dc20d08b272
kotaro <oratok.t@icloud.com>
2025-11-12 10:00:39 +0900

    name&age

---
+++ 20251112/nameage.js
@@ -0,0 +1,4 @@
+var myname = "Kotaro";
+var age = 21;
+console.log("\u540D\u524D\uFF1A ".concat(myname));
+console.log("\u5E74\u9F62\uFF1A ".concat(age));
---
+++ 20251112/nameage.ts
@@ -0,0 +1,4 @@
+const myname: string = "Kotaro";
+const age: number = 21;
+console.log(`名前： ${myname}`);
+console.log(`年齢： ${age}`);
